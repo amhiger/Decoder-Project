d87961a48449473dcf61545b8e3502c9601dacec
Adam Higer <86610022+amhiger@users.noreply.github.com>
2021-08-18 12:45:01 -0400

    Update substitution.js

--- src/substitution.js
+++ src/substitution.js
@@ -7,10 +7,12 @@ const substitutionModule = (function () {
   // you can add any code you want within this function scope
 const normalAlphabet = "abcdefghijklmnopqrstuvwxyz";
   function substitution(input, alphabet, encode = true) {
+//checks that the alphabet is 26 characters and a string
     if (!alphabet || alphabet.length < 26 || alphabet.length > 26 || typeof(alphabet)!= 'string')
     { 
       return false;
     }
+//checks that the alphabet has no duplicate letters
     for (let firstLetter in alphabet) {
       for (let matchingLetter in alphabet) {
         if (alphabet[firstLetter] === alphabet[matchingLetter] && firstLetter != matchingLetter) {
@@ -18,12 +20,15 @@ const normalAlphabet = "abcdefghijklmnopqrstuvwxyz";
         }
       }
     }
+//empty strings for decoding and encoding
     let encoded = "";
     let decoded = "";
+//contents of if statement encodes messages
     if (encode == true)
       {
         for(let i=0; i<input.length; i++)
           {
+//adds in spaces
             if (input[i] == " ")
               {
                 encoded += " ";
@@ -38,6 +43,7 @@ const normalAlphabet = "abcdefghijklmnopqrstuvwxyz";
           }
         return encoded;
       } 
+//contents of else statement decodes coded messages
     else
       {
         for(let i=0; i<input.length; i++)
