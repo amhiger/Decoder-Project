79fd7ca222906909533d559bda0848538c79e9f3
Adam Higer <86610022+amhiger@users.noreply.github.com>
2021-08-18 12:45:59 -0400

    Update polybius.js

--- src/polybius.js
+++ src/polybius.js
@@ -9,12 +9,14 @@ const cipher = {a:11,b:21,c:31,d:41,e:51,f:12,g:22,h:32,i:42,j:42,k:52,l:13,m:23
 const decipher = {
   11: "a",21: "b",31: "c",41: "d",51: "e",12: "f",22: "g",32: "h",42: "(i/j)",52: "k",13: "l",23: "m",33: "n",43: "o",53: "p",14: "q",24: "r",34: "s",44: "t",54: "u",15: "v",25: "w",35: "x",45: "y",55: "z",};
   function polybius(input, encode = true) {
+//sets input to lowercase
     input = input.toLowerCase();
     encodedMessage = "";
     if (encode == true)
     {
     for(let i = 0; i < input.length; i++) 
       {
+//handle spaces
         if(input[i]==" ")
           {
             encodedMessage += " ";
@@ -28,7 +30,7 @@ const decipher = {
     }
     else
     {
-      //let code = "";
+/*doubleSpaced string stores a copy of the input changing 1 space to 2 so that the string length stays an even number*/
       let decodedMessage = "";
       let doubleSpaced = "";
       for(let i = 0; i < input.length; i++)
@@ -47,7 +49,7 @@ const decipher = {
         return false;
       }
       for(let i = 0; i < doubleSpaced.length; i+=2)
-        {
+        {          
           if (doubleSpaced[i]!=" ")
           {
             let code = "";
@@ -55,6 +57,7 @@ const decipher = {
             code += doubleSpaced[i+1];
             decodedMessage += decipher[code];
           }
+//Changes double spaces to single space
           else
           {
             decodedMessage += " ";
